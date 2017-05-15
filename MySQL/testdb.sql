56928b1037d0ae5ca2ca8ed092c540c7a2ac9f07
Sarun <sarunchhoem@users.noreply.github.com>
2017-05-15 13:55:15 +0700

    add testdb.sql vai upload

---
+++ MySQL/testdb.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2017 at 08:54 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `idq` int(4) NOT NULL,
+  `answer` text COLLATE utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`idq`, `answer`) VALUES
+(1, 'Sarun'),
+(1, 'Sarun'),
+(2, 'freetime'),
+(2, 'freetime'),
+(2, ''),
+(4, 'Phanna is a girl'),
+(4, ''),
+(4, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+CREATE TABLE `question` (
+  `idq` int(4) NOT NULL,
+  `question` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`idq`, `question`) VALUES
+(1, 'What is your name?'),
+(2, 'What is your job?'),
+(3, 'where are you from?'),
+(4, 'what is phanna?'),
+(5, 'What is Information Technology?'),
+(6, 'where were city you born?');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testtable`
+--
+
+CREATE TABLE `testtable` (
+  `id` int(4) NOT NULL,
+  `name` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `age` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `testtable`
+--
+
+INSERT INTO `testtable` (`id`, `name`, `age`) VALUES
+(1, ' Hun bros ', 31),
+(22, 'Sarun', 34),
+(24, 'Sarun', 23),
+(31, 'Kombonrorn', 21),
+(43, 'yam', 23);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `answer`
+--
+ALTER TABLE `answer`
+  ADD KEY `idq` (`idq`);
+
+--
+-- Indexes for table `question`
+--
+ALTER TABLE `question`
+  ADD PRIMARY KEY (`idq`);
+
+--
+-- Indexes for table `testtable`
+--
+ALTER TABLE `testtable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `answer`
+--
+ALTER TABLE `answer`
+  ADD CONSTRAINT `answer_ibfk_1` FOREIGN KEY (`idq`) REFERENCES `question` (`idq`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
